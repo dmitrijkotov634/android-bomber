ecd38bc71252331f6faefb733cc043749a0d3f02
dmitrijkotov634 <dmitrijkotov634@gmail.com>
2021-07-17 17:26:36 +0300

    refactor: view binding

--- app/src/main/java/com/dm/bomber/ui/MainActivity.java
+++ app/src/main/java/com/dm/bomber/ui/MainActivity.java
@@ -5,28 +5,15 @@ import android.animation.AnimatorListenerAdapter;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.EditText;
-import android.widget.FrameLayout;
-import android.widget.LinearLayout;
-import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.dm.bomber.AttackManager;
 import com.dm.bomber.R;
-import com.google.android.material.chip.Chip;
-import com.google.android.material.progressindicator.CircularProgressIndicator;
+import com.dm.bomber.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-    private Spinner phoneCodeView;
-    private EditText phoneNumberView;
-    private EditText numberOfCyclesView;
-    private CircularProgressIndicator attackProgressView;
-    private Chip startAttackButton;
-
-    private FrameLayout darkLayout;
-    private LinearLayout attackScreenView;
-
+    private ActivityMainBinding binding;
     private AttackManager attackManager;
 
     private final String[] phoneCodes = {"7", "380", "375", ""};
@@ -34,29 +21,22 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        phoneCodeView = findViewById(R.id.country_select);
-        phoneNumberView = findViewById(R.id.phone_number);
-        numberOfCyclesView = findViewById(R.id.cycles_count);
-        attackProgressView = findViewById(R.id.progress);
-        startAttackButton = findViewById(R.id.start_attack);
-
-        darkLayout = findViewById(R.id.dark_layout);
-        attackScreenView = findViewById(R.id.attack_screen);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         CountryCodeAdapter adapter = new CountryCodeAdapter(this,
                 new int[]{R.drawable.ic_ru, R.drawable.ic_uk, R.drawable.ic_by, R.drawable.ic_all},
                 phoneCodes);
 
         String[] hints = getResources().getStringArray(R.array.hints);
-        phoneNumberView.setHint(hints[0]);
+        binding.phoneNumber.setHint(hints[0]);
 
-        phoneCodeView.setAdapter(adapter);
-        phoneCodeView.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        binding.countrySelect.setAdapter(adapter);
+        binding.countrySelect.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int index, long l) {
-                phoneNumberView.setHint(hints[index]);
+                binding.phoneNumber.setHint(hints[index]);
             }
 
             @Override
@@ -70,15 +50,15 @@ public class MainActivity extends AppCompatActivity {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        attackScreenView.setVisibility(View.GONE);
+                        binding.attackScreen.setVisibility(View.GONE);
 
-                        darkLayout.setAlpha(0.7f);
-                        darkLayout.animate()
+                        binding.fogging.setAlpha(0.7f);
+                        binding.fogging.animate()
                                 .alpha(0f)
                                 .setListener(new AnimatorListenerAdapter() {
                                     @Override
                                     public void onAnimationEnd(Animator animation) {
-                                        darkLayout.setVisibility(View.GONE);
+                                        binding.fogging.setVisibility(View.GONE);
                                         setEnabledMain(true);
                                     }
                                 });
@@ -91,14 +71,14 @@ public class MainActivity extends AppCompatActivity {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        attackProgressView.setMax(serviceCount * numberOfCycles);
-                        attackProgressView.setProgress(0);
+                        binding.progress.setMax(serviceCount * numberOfCycles);
+                        binding.progress.setProgress(0);
 
-                        darkLayout.setVisibility(View.VISIBLE);
-                        attackScreenView.setVisibility(View.VISIBLE);
+                        binding.fogging.setVisibility(View.VISIBLE);
+                        binding.attackScreen.setVisibility(View.VISIBLE);
 
-                        darkLayout.setAlpha(0f);
-                        darkLayout.animate()
+                        binding.fogging.setAlpha(0f);
+                        binding.fogging.animate()
                                 .alpha(0.7f)
                                 .setListener(null);
 
@@ -112,40 +92,40 @@ public class MainActivity extends AppCompatActivity {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        attackProgressView.setProgress(progress);
+                        binding.progress.setProgress(progress);
                     }
                 });
             }
         });
 
-        startAttackButton.setOnClickListener(new View.OnClickListener() {
+        binding.startAttack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String phoneNumber = phoneNumberView.getText().toString();
-                String numberOfCycles = numberOfCyclesView.getText().toString();
+                String phoneNumber = binding.phoneNumber.getText().toString();
+                String numberOfCycles = binding.cyclesCount.getText().toString();
 
                 if (phoneNumber.length() < 7) {
-                    phoneNumberView.setError(getString(R.string.length_error));
+                    binding.phoneNumber.setError(getString(R.string.length_error));
                     return;
                 }
 
                 int numberOfCyclesNum;
                 if (numberOfCycles.isEmpty() || numberOfCycles.equals("-") ||
                         (numberOfCyclesNum = Integer.parseInt(numberOfCycles)) < 0) {
-                    numberOfCyclesView.setError(getString(R.string.cycles_error));
+                    binding.cyclesCount.setError(getString(R.string.cycles_error));
                     return;
                 }
 
-                attackManager.performAttack(phoneCodes[phoneCodeView.getSelectedItemPosition()], phoneNumber, numberOfCyclesNum);
+                attackManager.performAttack(phoneCodes[binding.countrySelect.getSelectedItemPosition()], phoneNumber, numberOfCyclesNum);
             }
         });
     }
 
     public void setEnabledMain(boolean enabled) {
-        phoneNumberView.setEnabled(enabled);
-        phoneCodeView.setEnabled(enabled);
-        startAttackButton.setEnabled(enabled);
-        numberOfCyclesView.setEnabled(enabled);
+        binding.phoneNumber.setEnabled(enabled);
+        binding.cyclesCount.setEnabled(enabled);
+        binding.countrySelect.setEnabled(enabled);
+        binding.startAttack.setEnabled(enabled);
     }
 
     @Override
