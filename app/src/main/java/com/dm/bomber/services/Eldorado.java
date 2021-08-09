2af163a7e9ad5386c245ce874c80421d8b2e5097
Marat Nedogimov <nedo.marat@yandex.ru>
2021-08-09 23:45:10 +0500

    fix eldorado

--- app/src/main/java/com/dm/bomber/services/Eldorado.java
+++ app/src/main/java/com/dm/bomber/services/Eldorado.java
@@ -3,20 +3,36 @@ package com.dm.bomber.services;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import okhttp3.Request;
+
 public class Eldorado extends JsonService {
 
     public Eldorado() {
-        setUrl("https://www.eldorado.ru/_ajax/spa/auth/v2/auth_with_login.php");
+        setUrl("https://mobileapi.eldorado.ru/v1/user/auth/login");
         setMethod(POST);
         setPhoneCode("7");
+    }
+    
+    @Override
+    public Request buildRequest(Request.Builder builder) {
+        builder.addHeader("User-Agent", "Dalvik/2.1.0 (Linux; U; Android 10; Redmi Note 3 Build/QQ3A.200905.001)");
+
+        return super.buildRequest(builder);
     }
 
     @Override
     public String buildJson() {
         JSONObject json = new JSONObject();
+        JSONObject body_ = new JSONObject();
+        JSONObject head_ = new JSONObject();
 
         try {
-            json.put("user_login", format(getFormattedPhone(), "+* (***) *** ****"));
+            body_.put("userLogin", "+" + getFormattedPhone());
+            head_.put("uid", "unknown");
+            head_.put("os", "unknown");
+            head_.put("v", "unknown");
+            head_.put("FbToken", "unknown");
+            json.put("requestBody", body_);
         } catch (JSONException e) {
             e.printStackTrace();
         }
