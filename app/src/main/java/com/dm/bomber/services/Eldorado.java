1ae556f438e06db9ee03e1e7ab6e402defc89278
Marat Nedogimov <nedo.marat@yandex.ru>
2021-08-10 18:03:14 +0500

    update: fixes, add services...

--- app/src/main/java/com/dm/bomber/services/Eldorado.java
+++ app/src/main/java/com/dm/bomber/services/Eldorado.java
@@ -3,20 +3,37 @@ package com.dm.bomber.services;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import okhttp3.Request;
+
 public class Eldorado extends JsonService {
 
     public Eldorado() {
-        setUrl("https://www.eldorado.ru/_ajax/spa/auth/v2/auth_with_login.php");
+        setUrl("https://mobileapi.eldorado.ru/v1/user/auth/login");
         setMethod(POST);
         setPhoneCode("7");
+    }
+    
+    @Override
+    public Request buildRequest(Request.Builder builder) {
+        builder.addHeader("User-Agent", "Dalvik/2.1.0 (Linux; U; Android 10; Redmi Note 3 Build/QQ3A.200905.001)");
+
+        return super.buildRequest(builder);
     }
 
     @Override
     public String buildJson() {
         JSONObject json = new JSONObject();
+        JSONObject body = new JSONObject();
+        JSONObject head = new JSONObject();
 
         try {
-            json.put("user_login", format(getFormattedPhone(), "+* (***) *** ****"));
+            body.put("userLogin", "+" + getFormattedPhone());
+            head.put("uid", "unknown");
+            head.put("os", "unknown");
+            head.put("v", "unknown");
+            head.put("FbToken", "unknown");
+            json.put("requestBody", body);
+            json.put("requestHeader", head);
         } catch (JSONException e) {
             e.printStackTrace();
         }
---
+++ app/src/main/java/com/dm/bomber/services/FoodBand.java
@@ -0,0 +1,16 @@
+package com.dm.bomber.services;
+
+import okhttp3.HttpUrl;
+
+public class FoodBand extends ParamsService {
+
+    public FoodBand() {
+        setUrl("https://is.foodband.ru/api/rest/public/v2/customers/sendVerificationCode");
+    }
+
+    @Override
+    public void buildParams(HttpUrl.Builder builder) {
+        builder.addQueryParameter("phone", phone);
+        builder.addQueryParameter("token", "173f3678-b66c-4074-a280-f7df26009d26");
+    }
+}
